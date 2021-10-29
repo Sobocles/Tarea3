07322df3c215ea84cfd2fcaa1c0f0043dac36475
Sobocles <sebastian.morales.p01@mail.pucv.cl>
2021-10-29 03:53:53 -0300

    version 6

---
+++ src/app/formulario.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Contacto} from './contacto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormularioService {
+
+  constructor() {}
+    EnviarDatos(Datos:Contacto){
+      console.log(JSON.stringify(Datos));
+  }
+   
+}
